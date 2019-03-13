be0ec89246c7253b20ab05c1e2db66ab6217e095
ahnsangjae <sangjae2015@naver.com>
2019-03-13 22:34:56 +0900

    create

--- C++/AddTemp/AddTemp.h
+++ C++/AddTemp/AddTemp.h
@@ -12,12 +12,9 @@ T Add(T a, T b)
 template <> const char *Add<const char *>(const char *a, const char *b)
 {
 	char *temp_a = new char[strlen(a) + strlen(b) + 1];
-	char *temp_b = new char[strlen(b) + 1];
 
 	strcpy_s(temp_a, strlen(a) + 1, a);
-	strcpy_s(temp_b, strlen(b) + 1, b);
-
-	strcat_s(temp_a, strlen(temp_a) + strlen(temp_b) + 1, temp_b);
+	strcat_s(temp_a, strlen(temp_a) + strlen(b) + 1, b);
 
 	return temp_a;
 }
